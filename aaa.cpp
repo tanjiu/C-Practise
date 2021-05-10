644cebac29dabd5bc0975cb26f277992510615b3
tanjiu <228941381@qq.com>
2021-05-10 17:38:40 +0800

    提交aaa,cpp

--- aaa.cpp
+++ aaa.cpp
@@ -8,7 +8,7 @@ int main() {
     vector<int> a;
     a.push_back(5);
     cout << a[0] << endl;
-    cout << "我喜欢你!" << endl;
+    cout << "我喜欢你! 你喜欢我吗" << endl;
     system("pause");
     return 0;
 }
